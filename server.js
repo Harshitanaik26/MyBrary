3f1ddac022d3ef83139af7c546d360cf9589dfeb
Harshitanaik@26 <naikharshita93@gmail.com>
2021-06-19 19:18:06 +0530

    Initial Setup

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true
+    useUnifiedTopology:true
 })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
