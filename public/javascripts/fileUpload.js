310f81279e10e40b656638061775af9aeb936a43
Harshitanaik@26 <naikharshita93@gmail.com>
2021-07-10 20:01:44 +0530

    Aspect Ratio

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -2,5 +2,8 @@ FilePond.registerPlugin(FilePondPluginImagePreview);
 FilePond.registerPlugin(FilePondPluginImageResize);
 FilePond.registerPlugin(FilePondPluginFileEncode);
 
+FilePond.setOptions({
+    stylePanelAspectRatio: 150/100
+})
 
 FilePond.parse(document.body);
