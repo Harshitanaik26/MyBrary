2eabc98758f6f9e672316912ec9682f1f9f4dbfc
Harshitanaik@26 <naikharshita93@gmail.com>
2021-07-13 19:43:17 +0530

    File uploads database

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Book = require('../models/books')
 const Author = require('../models/author')
-const imageMimeTypes = ['image/jpeg' , 'image/png' , 'image/gifs']
+const imageMimeTypes = ['image/jpeg' , 'image/png' , 'image/gifs','image/jpg']
 
 
 //All Books route
