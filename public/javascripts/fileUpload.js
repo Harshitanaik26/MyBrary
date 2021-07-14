a919acb08e178d6b9daff3dcccde0ae4e1190b00
Harshitanaik@26 <naikharshita93@gmail.com>
2021-07-14 20:39:35 +0530

    File Upload

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,11 @@
-/*const { FilePond } = require("filepond")*/
-
-FilePond.registerPlugin(FilePondPluginImagePreview);
-FilePond.registerPlugin(FilePondPluginImageResize);
-FilePond.registerPlugin(FilePondPluginFileEncoded);
+FilePond.registerPlugin(FilePondPluginImagePreview)
+FilePond.registerPlugin(FilePondPluginImageResize)
+FilePond.registerPlugin(FilePondPluginFileEncoded)
 
 FilePond.setOptions({
     stylePanelAspectRatio: 150/100,
     imageResizeTargetWidth: 100,
     imageResizeTargetHeight: 150
-});
+})
 
-FilePond.parse(document.body);
+FilePond.parse(document.body)
