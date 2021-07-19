186a0e39aa46435938dee5562b5be0e7a7fa4b86
Harshitanaik@26 <naikharshita93@gmail.com>
2021-07-19 19:31:07 +0530

    Finished Author Routes

--- models/books.js
+++ models/books.js
@@ -38,7 +38,7 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function(){
-    if (this.coverImage != null && this.coverImageType != null) {
+    if (this.coverImage && this.coverImageType) {
         return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
 })
